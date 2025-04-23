70f0f5fc6fc7d4505fa64a16ad8c7d0f7682b1fd
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2025-04-23 15:19:52 +1000

    Edit the program to make it work as a pipe for other programs

--- lolcat/main.go
+++ lolcat/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"math"
 	"strings"
-	"syreclabs.com/go/faker"
+	"os"
+	"bufio"
 )
 
 func rgb(i int) (int, int, int) { 
@@ -15,19 +16,34 @@ func rgb(i int) (int, int, int) {
 		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
 }
 
-func main(){ 
-	var phrases []string 
-	
-	for i := 0; i < 3; i++ { 
-		phrases = append(phrases, faker.Hacker().Phrases()...)
-	}
-
-	output := strings.Join(phrases, "; ")
-	
-
+func print(output []rune ){
 	for j := 0; j < len(output); j++ { 
 		r, g, b := rgb(j)
 		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
 	}
 	fmt.Println()
+}
+
+func main(){ 
+	info, _ := os.Stdin.Stat()
+	
+	if info.Mode() & info.ModeCharDevice != 0 { 
+		fmt.Println("The command is intended to work with pipes.")
+        fmt.Println("Usage: fortune | gorainbow")
+	}
+
+	reader := bufio.reader(os.Stdin)
+	var output []rune 
+
+	for { 
+		input, _, err = reader.ReadRune()
+
+		if err != nil && err == io.EOF { 
+			break
+		}
+
+		output = append(output, input)
+	}
+
+	print(output)
 }
