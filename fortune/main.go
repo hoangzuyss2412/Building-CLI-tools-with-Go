d29ffb013e68958b2f54f03d7f04e3856be52cbc
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2025-04-28 23:42:19 +1000

    Refactor

--- fortune/main.go
+++ fortune/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os/exec"
 )
 
 func main() {
 	// Run "fortune -f" via WSL because we're on Windows
-	out, err := exec.Command("wsl", "fortune", "-f").CombinedOutput()
+	fortuneCommand = exec.Command("wsl", "fortune", "-f")
+
+	pipe, err = fortuneCommand.StderrPipe()
 	if err != nil {
 		panic(err)
 	}
+
+	fortuneCommand.Start()
+
+	outputStream := bufio.NewScanner()
+	outputStream.Scan()
+	fmt.Println(outputStream.Text())
 
 	fmt.Println(string(out))
 }
