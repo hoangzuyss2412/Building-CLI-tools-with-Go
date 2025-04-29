9aefba50ab5bca7e7dd509deae543c2f6c80af0c
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2025-04-29 22:34:35 +1000

    prints a random fortune filename on every run

--- fortune/main.go
+++ fortune/main.go
@@ -3,23 +3,88 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os/exec"
+	"runtime"
+	"math/rand"
+	"os"
+	"os/exec" 
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-func main() {
-	// Run "fortune -f" via WSL because we're on Windows
-	fortuneCommand = exec.Command("wsl", "fortune", "-f")
+var files []string
 
-	pipe, err = fortuneCommand.StderrPipe()
+func visit(path string, f os.FileInfo, err error) error { 
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(path, "/off/") { 
+		return nil 
+	}	
+
+	if filepath.Ext(path) == ".dat" { 
+		return nil
+	}
+
+	if f.IsDir() { 
+		return nil 
+	}
+
+	files = append(files, path)
+	return nil
+}
+
+func randomInt(min, max int) int { 
+	return min + rand.Intn(max - min)
+}
+
+func main() {
+	var fortuneCommand *exec.Cmd
+	
+	if runtime.GOOS == "windows" {
+		// Run "fortune -f" via WSL if we're on Windows
+		fortuneCommand = exec.Command("wsl", "fortune", "-f")
+	} else {
+		// Assume that we're only using Linux/MacOS
+		fortuneCommand = exec.Command("fortune", "-f")
+	}
+
+	pipe, err := fortuneCommand.StderrPipe()
 	if err != nil {
 		panic(err)
 	}
 
-	fortuneCommand.Start()
+	if err := fortuneCommand.Start(); err != nil { 
+		panic(err)
+	}
 
-	outputStream := bufio.NewScanner()
-	outputStream.Scan()
-	fmt.Println(outputStream.Text())
+	outputStream := bufio.NewScanner(pipe)
+	outputStream.Scan() 
+	line := outputStream.Text()
+	root := line[strings.Index(line, "/"):]
 
-	fmt.Println(string(out))
-}
+	// Convert Linux path to Windows path if running on Window machine
+	if runtime.GOOS == "windows" {
+		translate := exec.Command("wsl", "wslpath", "-w", root)
+		translatedBytes, err := translate.Output()
+		if err != nil {
+			panic(err)
+		}
+		windowsPath := strings.TrimSpace(string(translatedBytes))
+		err = filepath.Walk(windowsPath, visit)
+	} else { 
+		err = filepath.Walk(root, visit)
+	}
+	
+	if err != nil { 
+		panic(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	i := randomInt(1, len(files))
+	randomFile := files[i]
+	fmt.Println(randomFile)
+
+
+}	
