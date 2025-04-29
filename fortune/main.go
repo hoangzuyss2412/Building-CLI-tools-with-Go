af80d7dee6e8b3243a140dc5434fc9375607f3b6
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2025-04-29 23:37:29 +1000

    Complete fortune program

--- fortune/main.go
+++ fortune/main.go
@@ -3,45 +3,45 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"runtime"
+	"io"
 	"math/rand"
 	"os"
-	"os/exec" 
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
-	"time"
 )
 
 var files []string
 
-func visit(path string, f os.FileInfo, err error) error { 
+func visit(path string, f os.FileInfo, err error) error {
 	if err != nil {
 		return err
 	}
 
-	if strings.Contains(path, "/off/") { 
-		return nil 
-	}	
-
-	if filepath.Ext(path) == ".dat" { 
+	if strings.Contains(path, "/off/") {
 		return nil
 	}
 
-	if f.IsDir() { 
-		return nil 
+	if filepath.Ext(path) == ".dat" {
+		return nil
+	}
+
+	if f.IsDir() {
+		return nil
 	}
 
 	files = append(files, path)
 	return nil
 }
 
-func randomInt(min, max int) int { 
-	return min + rand.Intn(max - min)
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
 }
 
 func main() {
 	var fortuneCommand *exec.Cmd
-	
+
 	if runtime.GOOS == "windows" {
 		// Run "fortune -f" via WSL if we're on Windows
 		fortuneCommand = exec.Command("wsl", "fortune", "-f")
@@ -55,12 +55,12 @@ func main() {
 		panic(err)
 	}
 
-	if err := fortuneCommand.Start(); err != nil { 
+	if err := fortuneCommand.Start(); err != nil {
 		panic(err)
 	}
 
 	outputStream := bufio.NewScanner(pipe)
-	outputStream.Scan() 
+	outputStream.Scan()
 	line := outputStream.Text()
 	root := line[strings.Index(line, "/"):]
 
@@ -72,19 +72,33 @@ func main() {
 			panic(err)
 		}
 		windowsPath := strings.TrimSpace(string(translatedBytes))
-		err = filepath.Walk(windowsPath, visit)
-	} else { 
+		filepath.Walk(windowsPath, visit)
+	} else {
 		err = filepath.Walk(root, visit)
 	}
-	
-	if err != nil { 
+
+	if err != nil {
 		panic(err)
 	}
 
-	rand.Seed(time.Now().UnixNano())
 	i := randomInt(1, len(files))
 	randomFile := files[i]
-	fmt.Println(randomFile)
 
+	file, err := os.Open(randomFile)
+	if err != nil {
+		panic(err)
+	}
 
-}	
+	defer file.Close()
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	quotes := string(b)
+	quotesSlice := strings.Split(quotes, "%")
+	j := randomInt(1, len(quotesSlice))
+	fmt.Println(quotesSlice[j])
+
+}
