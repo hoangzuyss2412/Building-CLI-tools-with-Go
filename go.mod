27bde396f68921f3d095fcb275cbe689fc400c3f
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2025-04-30 16:17:45 +1000

    Remove unnecessary module and reformat code

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module github.com/hoangzuyss2412/cli-tools
 
 go 1.24.2
 
-require syreclabs.com/go/faker v1.2.3 // indirect
--- lolcat/main.go
+++ lolcat/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-func rgb(i int) (int, int, int) { 
+func rgb(i int) (int, int, int) {
 	f := 0.1
 
 	return int(math.Sin(f*float64(i)+0)*127 + 128),
@@ -16,30 +16,30 @@ func rgb(i int) (int, int, int) {
 		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
 }
 
-func print(output []rune ){
-	for j := 0; j < len(output); j++ { 
+func print(output []rune) {
+	for j := 0; j < len(output); j++ {
 		r, g, b := rgb(j)
 		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
 	}
 	fmt.Println()
 }
 
-func main(){ 
+func main() {
 	info, _ := os.Stdin.Stat()
-	
-	if (info.Mode() & os.ModeCharDevice) != 0 { 
+
+	if (info.Mode() & os.ModeCharDevice) != 0 {
 		fmt.Println("The command is intended to work with pipes.")
-        fmt.Println("Usage: fortune | gorainbow")
+		fmt.Println("Usage: fortune | gorainbow")
 	}
 
 	reader := bufio.NewReader(os.Stdin)
-	var output []rune 
+	var output []rune
 
-	for { 
+	for {
 		input, _, err := reader.ReadRune()
 
-		if err != nil { 
-			if err == io.EOF { 
+		if err != nil {
+			if err == io.EOF {
 				break
 			}
 
